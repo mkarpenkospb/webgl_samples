7b83c110e873f02b7b7a3c237e1ba72e6cb1992c
mkarpenko <mkarpenko.spb@mail.ru>
2020-11-17 00:05:59 +0300

    add simple hand-made controls

--- src/PearArea.jsx
+++ src/PearArea.jsx
@@ -18,6 +18,17 @@ function optionColorToVec3(color) {
     return new THREE.Vector3(values[0] / 255, values[1] / 255, values[2] / 255);
 }
 
+function getVideoCardInfo(gl) {
+    const debugInfo = gl.getExtension('WEBGL_debug_renderer_info');
+    return debugInfo ? {
+        vendor: gl.getParameter(debugInfo.UNMASKED_VENDOR_WEBGL),
+        renderer:  gl.getParameter(debugInfo.UNMASKED_RENDERER_WEBGL),
+    } : {
+        error: "no WEBGL_debug_renderer_info",
+    };
+}
+
+
 export class ViewArea extends Component {
     constructor() {
         super();
@@ -81,27 +92,61 @@ export class ViewArea extends Component {
             a: 0.4,
             rotationSpeed: 60
         };
+
+        // константы для мыши
+
+
+        this.mouseControls  = {
+            followMouse: false,
+            xStartPos: 0,
+            yStartPos: 0,
+            xPos: 0,
+            yPos: 0,
+            deltaX: 0,
+            deltaY: 0
+        }
     }
 
     componentDidMount() {
         const canvas = this.canvasRef.current;
+        let Y_AXIS = new THREE.Vector3( 0, 1, 0 );
         if (!canvas) {
             return;
         }
+        canvas.width = 1920;
+        canvas.height = 1080;
+        // this.controls = new THREE.OrbitControls(this.camera, this.canvasRef.current);
+        // this.controls.update();
 
-        this.controls = new THREE.OrbitControls(this.camera, this.canvasRef.current);
-        this.controls.update();
+        const gl = canvas.getContext('webgl2',
+            { powerPreference : "high-performance"}
 
-        const gl = canvas.getContext('webgl2');
+            );
         if (!gl) {
             return;
         }
+        // gl.device
+        console.log(getVideoCardInfo(gl));
+        // gl.powerPreference
 
+        // then(function (devices) {
+        //     devices.forEach(function (device) {
+        //         console.log(device.kind + ": " + device.label + " id = " + device.deviceId);
+        //     });
+        // });
         this.addDatGUI();
         let renderer = new THREE.WebGLRenderer({canvas: canvas, context: gl});
-        renderer.setSize(canvas.width, canvas.height);
+        let ratio = window.devicePixelRatio;
+        renderer.setSize(window.width, window.height);
+        renderer.setPixelRatio(ratio);
 
         this.prevTime = new Date();
+
+        canvas.addEventListener('mousedown', this.onMouseDown, false);
+        canvas.addEventListener('mouseup', this.onMouseUp, false);
+        canvas.addEventListener('mousemove', this.onMouseMove, false);
+
+
 
         const renderLoopTick = () => {
             // Handle resize
@@ -127,8 +172,20 @@ export class ViewArea extends Component {
                 this.camera.position.set(d.x, d.y, d.z);
                 this.camera.quaternion.clone(q);
                 this.camera.scale.set(s.x, s.y, s.z);
-                this.controls = new THREE.OrbitControls(this.camera, canvas);
+                // this.controls = new THREE.OrbitControls(this.camera, canvas);
             }
+
+            // support camera controls
+
+            if (this.mouseControls.followMouse) {
+                this.mouseControls.deltaX += (this.mouseControls.xPos - this.mouseControls.xStartPos) * 0.01;
+                this.mouseControls.deltaY += (this.mouseControls.yPos - this.mouseControls.yStartPos) * 0.01;
+                this.mouseControls.xStartPos = this.mouseControls.xPos;
+                this.mouseControls.yStartPos = this.mouseControls.yPos;
+
+            }
+
+            this.camera.rotateOnAxis(Y_AXIS, this.mouseControls.deltaX);
 
             const curTime = new Date();
 
@@ -136,10 +193,10 @@ export class ViewArea extends Component {
             gl.clearColor(0.2, 0.2, 0.2, 1.0);
             gl.clear(gl.COLOR_BUFFER_BIT);
 
-            this.controls.update();
+            // this.controls.update();
 
             this.pearObject.position.set(5, 2, 2);
-            this.pearObject.scale.set(3, 3, 3);
+            this.pearObject.scale.set(1, 1, 1);
             this.pearRotation = this.pearRotation + (curTime.getTime() - this.prevTime.getTime()) / 1000 * this.options.rotationSpeed * Math.PI / 180;
             this.pearObject.quaternion.setFromAxisAngle(new THREE.Vector3(0, 1, 0), this.pearRotation);
 
@@ -167,6 +224,30 @@ export class ViewArea extends Component {
         fields.add(this.options, "a", 0, 1, 0.01);
         fields.open();
     }
+
+
+    updateCursorPos = (event) => {
+        let rect = this.canvasRef.current.getBoundingClientRect();
+        this.mouseControls.xStartPos = event.clientX - rect.left;
+        this.mouseControls.yStartPos = event.clientY - rect.top;
+    }
+
+    onMouseDown = (event) => {
+        this.mouseControls.followMouse = true;
+        this.updateCursorPos(event);
+    }
+
+    onMouseUp = (event) => {
+        this.mouseControls.followMouse = false;
+        this.updateCursorPos(event);
+    }
+
+    onMouseMove = (event) => {
+        let rect = this.canvasRef.current.getBoundingClientRect();
+        this.mouseControls.xPos = event.clientX - rect.left;
+        this.mouseControls.yPos = event.clientY - rect.top;
+    }
+
 
     render() {
         return (
